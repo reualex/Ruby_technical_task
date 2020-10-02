fb7ed55552d0d266b1218aa41bb7fe33b85b1fa2
Reuka Alex <a.reuka@leviossa.net>
2020-10-02 18:51:28 +0300

    add error proccesing

--- app.rb
+++ app.rb
@@ -1,53 +1,56 @@
 class Checkout
- require './items.rb'
- require './rules.rb'
+  require './items.rb'
+  require './rules.rb'
 
-def initialize(rules)
-  @rules = rules
-end
 
-def scan(item)
-  @items ||= {}
-  @item ||= {}
-  @item = {
-    price: item.price,
-    count: 1,
-    total_price: item.price,
-  }
-
-  unless @items.has_key?(item.type)
-    @items = @items.merge({item.type => @item})
-  else
-    @items[item.type][:count] += 1
-    @items[item.type][:total_price] += item.price
-  end
-  proceed_discount(@rules[item.type.to_sym], @items[item.type])
-end
-
-def proceed_discount(rule, item)
-  unless (rule.nil?)
-      if item[:count].send(rule[:condition_method], rule[:condition]).zero?
-        item[:total_price] = item[:total_price].send(rule[:operation], rule[:discount])
-      end
+  def initialize(rules)
+    @rules = rules
+    if @rules[:error]
+      abort("#{@rules[:error]}")
     end
   end
 
-def general_rules(items, total)
-  rule = @rules[:total]
-  items.values.each do |item|
-    total += item[:total_price]
+  def scan(item)
+    @items ||= {}
+    @item ||= {}
+    @item = {
+      price: item.price,
+      count: 1,
+      total_price: item.price,
+    }
+    unless @items.has_key?(item.type)
+      @items = @items.merge({item.type => @item})
+    else
+      @items[item.type][:count] += 1
+      @items[item.type][:total_price] += item.price
+    end
+    proceed_discount(@rules[item.type.to_sym], @items[item.type])
   end
 
-  if total.send(rule[:condition_method], rule[:condition])
-    return total = total.send(rule[:operation], rule[:discount])
-  else
-    return total
+  def proceed_discount(rule, item)
+    unless (rule.nil?)
+        if item[:count].send(rule[:condition_method], rule[:condition]).zero?
+          item[:total_price] = item[:total_price].send(rule[:operation], rule[:discount])
+        end
+      end
+    end
+
+  def general_rules(items, total)
+    rule = @rules[:total]
+    items.values.each do |item|
+      total += item[:total_price]
+    end
+    if total.send(rule[:condition_method], rule[:condition])
+      return total = total.send(rule[:operation], rule[:discount])
+    else
+      return total
   end
 end
 
-def total
-  @total ||= 0
-  @total = general_rules(@items, @total)
-end
+  def total
+    @total ||= 0
+    @total = general_rules(@items, @total)
+  end
+
 
 end
